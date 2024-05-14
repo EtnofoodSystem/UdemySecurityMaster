2919e56948069d9b1ff9c8d5b3dd1e2ef8371d69
EtnofoodSystem <sistemas@etnofood.org>
2024-05-14 13:20:42 -0600

    Impl jwt service y mas 3

--- src/main/java/com/api/course/service/UserServiceImpl.java
+++ src/main/java/com/api/course/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.api.course.entity.util.Role;
 import com.api.course.exception.InvalidPasswordException;
 import com.api.course.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -13,13 +14,15 @@ import org.springframework.util.StringUtils;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public User registrOneCustomer(SaveUser newUser) {
         validationPassword(newUser);
         User user = new User();
         user.setName(newUser.getName());
         user.setUsername(newUser.getUsername());
-        user.setPassword(newUser.getPassword());
+        user.setPassword(passwordEncoder.encode(newUser.getPassword()));
         user.setRole(Role.ROLE_CUSTOMER);
         return userRepository.save(user);
     }
