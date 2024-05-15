602dac131d4d654e1c9d65b0669cfb27cffea494
EtnofoodSystem <sistemas@etnofood.org>
2024-05-15 15:16:46 -0600

    Impl jwt service y mas 5

--- src/main/java/com/api/course/service/UserService.java
+++ src/main/java/com/api/course/service/UserService.java
@@ -3,7 +3,7 @@ package com.api.course.service;
 import com.api.course.dto.SaveUser;
 import com.api.course.entity.User;
 
-public interface UserService {
+public interface  UserService {
 
     User registrOneCustomer(SaveUser newUser);
 }
