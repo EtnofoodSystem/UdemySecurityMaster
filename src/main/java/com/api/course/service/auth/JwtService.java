85355953a290b5566989a24d53da767b7f6e456b
EtnofoodSystem <sistemas@etnofood.org>
2024-05-15 16:47:33 -0600

    Service JWT

--- src/main/java/com/api/course/service/auth/JwtService.java
+++ src/main/java/com/api/course/service/auth/JwtService.java
@@ -1,12 +1,37 @@
 package com.api.course.service.auth;
 
 import com.api.course.entity.User;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class JwtService {
-    public String generatedToken(UserDetails user) {
-        return null;
+    @Value("${security.jwt.expiration-in-minutes}")
+    private Long EXPIRATION_IN_MINUTES;
+    public String generatedToken(UserDetails user,Map<String, Object> extraClaims) {
+        Date issuedAt = new Date(System.currentTimeMillis());
+        Date expiration = new Date(issuedAt.getTime() + (EXPIRATION_IN_MINUTES * 60 * 1000));
+        String jwt = Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .setHeader(Header.TYPE,Header.JWT_TYPE)
+                .signWith(generatedKey(), SignatureAlgorithm.HS256)
+                .compact()
+                ;
+        return jwt;
+    }
+
+    private Key generatedKey() {
     }
 }
