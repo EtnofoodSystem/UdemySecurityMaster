a8793ec6eb17b259ec86a23268f6902db99ec937
EtnofoodSystem <sistemas@etnofood.org>
2024-05-21 17:00:42 -0600

    No se que cambio ocurrio pero fue en el authenticationcontroller

--- src/main/java/com/api/course/controller/AuthenticationController.java
+++ src/main/java/com/api/course/controller/AuthenticationController.java
@@ -5,7 +5,6 @@ import com.api.course.dto.AuthenticationResponse;
 import com.api.course.entity.User;
 import com.api.course.service.auth.AuthenticationService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
